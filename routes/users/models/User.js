6dbb3861a1bcb2a24197652e3017a4d9d85a9b29
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-07-15 23:41:40 -0400

    updated add goal ejs

--- routes/users/models/User.js
+++ routes/users/models/User.js
@@ -38,6 +38,7 @@ UserSchema.pre('save' , function(next){
 
     bcrypt.genSalt(10 , (err , salt) => {
         if (err) return next(err)
+
         bcrypt.hash(user.password , salt , (err , hash) => {
             if (err) return next(err)
             user.password = hash
