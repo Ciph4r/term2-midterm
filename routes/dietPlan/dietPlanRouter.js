0aceff4c73ee25f1a1fb3533004a6632b11a9da7
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-07-16 13:52:16 -0400

    test routes working

--- routes/dietPlan/dietPlanRouter.js
+++ routes/dietPlan/dietPlanRouter.js
@@ -3,20 +3,46 @@ const router = express.Router();
 const DietPlan = require('./models/dietPlan')
 const auth = require('../middleware/auth');
 const app = require('../../app');
+const axios = require('axios')
+require('dotenv').config()
 
 
 
-router.post('/add-diet' , async (req,res,next) => {
+
+
+router.get('/' , (req,res,next) => {
+    return res.render('auth/addDiet')
+})
+
+// router.post('/add-diet' , async (req,res,next) => {
+//     try {
+//         let dietPlan = await new dietPlan()
+//         dietPlan.owner = req.user._id
+//         await dietPlan.save()
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+// })
+//////////////////////////////////////   test routes
+router.get('/findfood' , async (req,res,next) => {
     try {
-        let dietPlan = await new dietPlan()
-        dietPlan.owner = req.user._id
-        await dietPlan.save()
+        const search = req.body.search.split(' ').join('%')
+        const response = await axios.get(`https://api.edamam.com/api/food-database/v2/parser?ingr=${search}&app_id=${process.env.FOOD_ID}&app_key=${process.env.FOOD_KEY}`)
+       
+        console.log(response.data)
+            return res.send(response.data)   
     }
-    catch(err){
-        console.log(err)
+    catch(err) {
+    console.log(err)
     }
 })
 
+// router.post('/findfood' , (req,res,next) => {
+
+
+// })
+
 
   module.exports = router;
-  
+
