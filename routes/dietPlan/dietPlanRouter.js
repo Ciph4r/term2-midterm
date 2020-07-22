791aaaf2ee7100c07b395d3c3c15efd415c473ca
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-07-21 23:41:02 -0400

    diet controller updated

--- routes/dietPlan/dietPlanRouter.js
+++ routes/dietPlan/dietPlanRouter.js
@@ -4,12 +4,10 @@ const DietPlan = require('./models/dietPlan')
 const User = require('../users/models/User')
 const Meals = require('../meals/models/Meals')
 const auth = require('../middleware/auth');
-const moment = require('moment')
-const app = require('../../app');
-const axios = require('axios');
 const { query } = require('express');
 const Meal = require('../meal/models/Meal');
 const { check , validationResult } = require('express-validator');
+const {addDiet, findFood , addFood} = require('./controller/dietPlanController.')
 require('dotenv').config()
 
 
@@ -43,37 +41,7 @@ router.get('/' , (req,res,next) => {
 
 })
 
-router.post('/add-diet' , dietValidation, async (req,res,next) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        req.flash('errors' , errors.array()[0].msg)
-        return res.redirect('back')
-  
-    }
-    try {
-        let diet = await DietPlan.find({date: req.body.mealDate })
-        if (diet.length){
-            req.flash('errors' , " this date already exist")
-            return res.redirect('back')
-        }
-        let dietPlan = await new DietPlan()
-        dietPlan.owner = req.user._id
-        dietPlan.date = req.body.mealDate
-        for (let i = 0 ; i < req.body.mealQuantity ; i++){
-            let meals = await new Meals()
-            dietPlan.meals.push(meals._id)
-            meals.owner = dietPlan._id
-            meals.time= req.body[`mealTime${i}`] 
-            await meals.save()
-        }       
-                await dietPlan.save()
-                return res.redirect('/api/v1/dietPlan/show-meals/'+ dietPlan._id)
-    }
-    catch (err){
-        console.log(err)
-    }
-  
-})
+router.post('/add-diet' , dietValidation, addDiet)
 
 router.get('/add-meal' , (req,res,next) => {
     return res.render('auth/addMeals')
@@ -101,68 +69,10 @@ router.get('/food-search/:meals_id' ,(req,res,next) => {
 
 
 
-router.get('/findfood', searchValidation ,async (req,res,next) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        req.flash('errors' , errors.array()[0].msg)
-        return res.redirect('back')
-  
-    }
-
-        try {
-            let foundMeal = await Meals.findOne({_id: req.query.mealId})
-  
-        const search = req.query.search.split(' ').join('%20')
-        const response = await axios.get(`https://api.edamam.com/api/food-database/v2/parser?ingr=${search}&app_id=${process.env.FOOD_ID}&app_key=${process.env.FOOD_KEY}`)
-            if(!response.data.parsed.length){
-                req.flash('errors', `CANT FIND ${req.query.search}`)
-                return res.redirect('back')
-            }
-       
-        let data = await response.data
-        return res.render('auth/foodSearch' , {data: data , foundMeal})
-      
-          
-    }
-    catch(err) {
-    console.log(err)
-    }
-})
+router.get('/findfood', searchValidation ,findFood)
 
 
-router.put('/add-food' , (req,res,next) => {
-    const {calories , fat , protein , name, serving} = req.body
-    const mealId = req.body.mealId
-    Meals.findOne({_id : mealId}).then((foundMeal) => {
-    foundMeal.items.push({
-      name,
-      calories,
-      fat,
-      protein,
-      serving
-     })
-    foundMeal.save().then(()=> {
-        res.redirect('back')
-    })            
-               
-    })
-    .catch((err) => console.log(err))
-    
-})
-router.put('/remove-item/:meal_id' , async (req,res,next) => {
-    try {
-        let meal = await Meals.findOne({_id:req.params.meal_id })
-        meal.items.splice(req.body.index,1)
-        
-        await meal.save()
-        req.flash('success' , 'Item Remove')
-        res.redirect('back')
-    }
-    catch(err){
-        console.log(err)
-    }
-   
-})
+router.put('/add-food' , addFood)
 
 
 
